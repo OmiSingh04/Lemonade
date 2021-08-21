e43c9083fa2329b6b8d36a07b6dcabaf129f2513
OmiSingh2004 <omisinghdbz@gmail.com>
2021-08-21 23:28:21 +0530

    add bruh

---
+++ src/main/java/dev/os/damon/gilbert/App.java
@@ -0,0 +1,53 @@
+package dev.os.damon.gilbert;
+
+import javax.security.auth.login.LoginException;
+
+import emotes.HelloEvent;
+import games.CoinFlip;
+import games.DiceRoll;
+import lemonade.util.InfoEvent;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.utils.Compression;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import util.AvatarEvent;
+import util.ClearEvent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public static final String SERVER_PREFIX = "LM.";
+
+	public static void main(String[] args) throws LoginException {
+		JDABuilder builder = JDABuilder.createDefault(App.SERVER_PREFIX);
+
+		// Disable parts of the cache
+		builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
+		// Enable the bulk delete event
+		builder.setBulkDeleteSplittingEnabled(false);
+		// Disable compression (not recommended)
+		builder.setCompression(Compression.NONE);
+		// builder.setStatus(OnlineStatus.IDLE).setActivity(Activity.watching("your
+		// winstreaks..."));
+		JDA jda = builder.build();
+		jda.addEventListener(new HelloEvent());
+		jda.addEventListener(new AvatarEvent());
+		jda.addEventListener(new ClearEvent());
+		jda.addEventListener(new InfoEvent());
+		jda.addEventListener(new CoinFlip());
+		jda.addEventListener(new DiceRoll());
+	}
+
+}
+
+/*
+ * 
+ * private String URLConnection = "jdbc:mysql://localhost:3306/lemonade_db";
+ * private String userName = "root"; private String password = "pizza@eater";
+ * 
+ * 
+ * 
+ */
---
+++ src/main/java/games/CoinFlip.java
@@ -0,0 +1,24 @@
+package games;
+
+
+import dev.os.damon.gilbert.App;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class CoinFlip extends ListenerAdapter{
+	
+	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+		String message = event.getMessage().getContentRaw().toString().toUpperCase();
+		
+		if(message.startsWith(App.SERVER_PREFIX + "FLIP")) {
+			EmbedBuilder embed = new EmbedBuilder();
+			int n = Integer.parseInt(Character.toString(Double.toString(Math.random()).charAt(17)));
+			if(n < 5)	embed.setDescription("Heads!");
+			else	embed.setDescription("Tails!");
+			event.getChannel().sendMessage(embed.build()).queue();
+			
+		}
+	}
+
+}
